188f23973789c8b168aca6ce37ff15809303dbb3
Alison Lu <alibob12401@hotmail.com>
2022-06-06 17:00:01 -0400

    simplified file saving to function

--- project/graphics/examples/bokeh-barchart.py
+++ project/graphics/examples/bokeh-barchart.py
@@ -3,31 +3,22 @@ from bokeh.plotting import figure
 from selenium import webdriver
 import chromedriver_binary
 
+def save (p):
+    name = __file__.replace(".py", "")
+    export_png(p, filename=f"images/{name}.png")
+    export_svg(p, filename=f"images/{name}.svg")
+    show(p)
 
 data = {'Rock': 136, 'Rap': 112, 'Folk': 110, 'Indie': 90, 'Jazz': 25}
 x = list(data.keys())
 y = list(data.values())
-z = [1,2,3]
 
 p = figure(x_range = x, title="Bar Chart")
-
-print(type(x))
-print(type(y))
-print(type(z))
-print(x)
-print(y)
-print(z)
 
 p.vbar(x=x, top = y, line_color = 'black',color='orange',
     width = 0.9,
     line_width = 2
        )
 
-
-
-show(p)
-
-export_png(p, filename="images/bokeh-bar.png")
-export_svg(p, filename="images/bokeh-bar.svg")
-
+save(p)
 
