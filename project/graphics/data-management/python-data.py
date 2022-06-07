ccfc642c61a7dd8a90240452f752a4c5b2b352c9
Jackson Miskill <jcmiskill@gmail.com>
2022-06-07 16:38:38 -0400

    Nothing

--- project/graphics/data-management/python-data.py
+++ project/graphics/data-management/python-data.py
@@ -3,7 +3,13 @@ Purpose: python uses several different data structures. It is important to know 
          The following is used to demonstrate how the structures work and how to use them to create the necessary
          visuals for data analysis.
 
-This code is available on GitHub at the following address:
+This code is available on GitHub at the following address: https://github.com/cybertraining-dsc/reu2022/tree/main/project/graphics/data-management
 
 """
 
+# Lists
+
+x = []
+y = []
+for i in range(0, 100):
+    x.append()
