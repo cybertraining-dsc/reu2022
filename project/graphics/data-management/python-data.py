462d6936acabe6676b23f3d016e9e29d5ff924fc
Gregor von Laszewski <laszewski@gmail.com>
2022-06-08 20:45:33 -0400

    reformat and remove some markdown errors

--- project/graphics/data-management/python-data.py
+++ project/graphics/data-management/python-data.py
@@ -5,11 +5,15 @@ from matplotlib import pyplot as plt
 import seaborn as sns
 
 """
-Purpose: python uses several different data structures. It is important to know how to operate these data structures.
-         The following is used to demonstrate how the structures work and how to use them to create the necessary
-         visuals for data analysis.
 
-This code is available on GitHub at the following address: https://github.com/cybertraining-dsc/reu2022/tree/main/project/graphics/data-management
+Purpose: python uses several different data structures. It is
+         important to know how to operate these data structures.  The
+         following is used to demonstrate how the structures work and
+         how to use them to create the necessary visuals for data
+         analysis.
+
+This code is available on GitHub at the following address:
+https://github.com/cybertraining-dsc/reu2022/tree/main/project/graphics/data-management
 
 """
 
@@ -38,12 +42,13 @@ height_data = []
 weight_data = []
 
 with open('/Users/jacksonmiskill/Downloads/biostats.csv',
-          'r') as file:  # opens the csv and creates the reader object for it
+          'r') as file:
+    # opens the csv and creates the reader object for it
     reader = csv.reader(file, delimiter=',')
 
     for each_row in reader:
-        if each_row:  # you have to check for blank lines within the document
-
+        if each_row:
+            # you have to check for blank lines within the document
             names.append(each_row[0])
             sex_data.append(each_row[1])
             age_data.append(each_row[2])
@@ -60,7 +65,8 @@ print(height_data)
 
 
 
-# let's plot the heights and weight of everyone. We could maybe use a hue here to denote who it is
+# let's plot the heights and weight of everyone. We could maybe use a
+# hue here to denote who it is
 plt.plot(names, height_data)
 plt.xlabel("Name")
 plt.ylabel("Height")
