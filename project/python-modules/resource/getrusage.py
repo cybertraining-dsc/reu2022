7dc8b0bbc10960dace3699c3f89884d09018690d
Gregor von Laszewski <laszewski@gmail.com>
2022-06-09 19:20:58 -0400

    new resources

--- project/python-modules/resource/getrusage.py
+++ project/python-modules/resource/getrusage.py
@@ -12,6 +12,27 @@ RESOURCES = [
     ('ru_oublock', 'Block outputs'),
 ]
 
+"""
+Gregor suggests:
+RESOURCES = [
+("ru_utime", "time in user mode (float seconds)"),
+("ru_stime", "time in system mode (float seconds)"),
+("ru_maxrss", "maximum resident set size"),
+("ru_ixrss", "shared memory size"),
+("ru_idrss", "unshared memory size"),
+("ru_isrss", "unshared stack size"),
+("ru_minflt", "page faults not requiring I/O"),
+("ru_majflt", "page faults requiring I/O"),
+("ru_nswap", "number of swap outs"),
+("ru_inblock", "block input operations"),
+("ru_oublock", "block output operations"),
+("ru_msgsnd", "messages sent"),
+("ru_msgrcv", "messages received"),
+("ru_nsignals", "signals received"),
+("ru_nvcsw", "voluntary context switches"),
+("ru_nivcsw", "involuntary context switches"),
+"""
+
 usage = resource.getrusage(resource.RUSAGE_SELF)
 
 for name, desc in RESOURCES:
