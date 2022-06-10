fa34b38df165fd7665c17d48348ae0715a56bd88
Jackson Miskill <jcmiskill@gmail.com>
2022-06-10 13:57:26 -0400

    updated some files into order to pull

--- project/graphics/examples/compare2.0.py
+++ project/graphics/examples/compare2.0.py
@@ -1,8 +1,21 @@
 import pandas as pd
 from cloudmesh.common.util import writefile
+import yaml
+from pprint import pprint
+
 
 filename = 'compare-graphics'
 df = pd.read_csv(f'{filename}.csv')
+print(df)
+df = df.set_index('Value')
+df = df.transpose()
 
 content = df.to_markdown()
 writefile(f'{filename}.md', content)
+
+d = df.to_dict(orient='list')
+pprint(d)
+
+
+with open(f'{filename}.yaml', 'w') as file:
+    documents = yaml.dump(d, file)
