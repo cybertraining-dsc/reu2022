ebbc43d3fe152593bf00aeb7d744c025d5489434
al07734 <alibob12401@hotmail.com>
2022-07-14 14:41:45 -0400

    run make and update higgs d

--- code/deeplearnning/E534_Higgs_Discovery_D.py
+++ code/deeplearnning/E534_Higgs_Discovery_D.py
@@ -5,11 +5,16 @@
 # 
 # This notebook discusses aspects of random number generation including seeds.
 
+
+
 try:
     from cloudmesh.common.StopWatch import StopWatch
 except:  # noqa: E722
     get_ipython().system(' pip install cloudmesh-common')
     from cloudmesh.common.StopWatch import StopWatch
+
+
+
 
 StopWatch.start("total")
 StopWatch.start("import")
@@ -17,12 +22,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 StopWatch.stop("import")
 
+
 # ## Part 1 Fixing Seed, fixes Random Numbers
 # 
 # Create two sets of random numbers with the same seed(1234567) and plot them on top of each other. As identical sets of random numbers are created with identical seeds, you will get two plots exactly on top of each other.
 
 
-StopWatch.start("data-create")
+
+StopWatch.start("Part 1")
+StopWatch.start("data-create-1")
 np.random.seed(seed=1234567)                                            #set the seed of the random number generator to 1234567
 Base = 110 + 30* np.random.rand(42000)                                  #generate 42000 random numbers between 110 and 140
 plt.figure("On Top of Each Other")
@@ -30,9 +38,13 @@ plt.hist(Base, bins=15, range =(110,140), alpha = 0.5, color="blue")    #Plot th
 
 np.random.seed(seed=1234567)                                            #set the seed of the random number generator to 1234567 for the second set
 Base2 = 110 + 30* np.random.rand(42000)                                 #Generate the second set of random numbers
+StopWatch.stop("data-create-1")
+StopWatch.start("data-plot-1")
 plt.hist(Base2, bins=15, range =(110,140), alpha = 0.5, color="yellow") #Plot the histogram for the second set
 plt.title("Two histograms on top of each other as identical random numbers")
 plt.show()
+StopWatch.stop("data-plot-1")
+StopWatch.stop("Part 1")
 
 
 # ## Part 2 Varying Seed (randomly) gives distinct results
@@ -41,6 +53,7 @@ plt.show()
 
 
 
+StopWatch.start("Part 2")
 plt.figure("Different")
 np.random.seed(seed=1234567)                                            #set the seed of the random number generator to 1234567 for the first set of random numbers
 Base4 = 110 + 30* np.random.rand(42000)                                 #Generate a set of 42000 random numbers between 110 and 140(first set)
@@ -53,4 +66,6 @@ plt.hist(Base3, bins=15, range =(110,140), alpha = 0.5, color="red")    #Plot th
 plt.title("Three distinct histograms as different random numbers")
 # For Agg backend
 plt.show()
+StopWatch.stop("Part 2")
+StopWatch.stop("total")
 
