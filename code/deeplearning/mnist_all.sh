18e40c0215f5e5b1673d361fb1b3a0155fa50e4d
Alex Beck <abeckct@gmail.com>
2022-07-22 17:05:13 -0400

    Deleted .py files to update .ipynb and created script to run all testssh: wq: command not found

---
+++ code/deeplearning/mnist_all.sh
@@ -0,0 +1,11 @@
+sbatch mlp_mnist.sh
+sbatch mnist_autoencoder.sh
+sbatch mnist_cnn.sh
+sbatch mnist_lstm.sh
+sbatch mnist_mlp_with_lstm.sh
+sbatch mnist_rnn.sh
+sbatch mnist_with_distributed_training.sh
+sbatch mnist_with_pytorch.sh
+
+
+
