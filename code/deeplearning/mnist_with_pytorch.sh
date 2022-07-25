aae14e143aa24b59125d7a9d52921c97c8f8bae6
Alex Beck <abeckct@gmail.com>
2022-07-25 10:48:17 -0400

    Added sh and py files sh: wq: command not found

---
+++ code/deeplearning/mnist_with_pytorch.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#SBATCH --job-name=mnist_with_pytorch.sh
+#SBATCH --output=mnist_with_pytorch.log
+#SBATCH --error=mnist_with_pytorch.error
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:v100:1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=4GB
+#SBATCH --time=3:00
+
+echo "# cloudmesh status=running progress=1 pid=$$"
+
+nvidia-smi --list-gpus
+source activate ENV3
+python mnist_with_pytorch.py
+
+echo " cloudmesh status=done progress=100 pid=$$"#
---
+++ code/deeplearning/python_warmup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#SBATCH --job-name=python_warmup.sh
+#SBATCH --output=python_warmup.log
+#SBATCH --error=python_warmup.error
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:v100:1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=4GB
+#SBATCH --time=3:00
+
+echo "# cloudmesh status=running progress=1 pid=$$"
+
+nvidia-smi --list-gpus
+source activate ENV3
+python python_warmup.py
+
+echo " cloudmesh status=done progress=100 pid=$$"#
