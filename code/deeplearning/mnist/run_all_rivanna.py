8888925312673ff0409f39ecd9f483749bf5264b
Alex Beck <abeckct@gmail.com>
2022-07-26 15:37:35 -0400

    Updated Stopwatch... again

--- code/deeplearning/mnist/run_all_rivanna.py
+++ code/deeplearning/mnist/run_all_rivanna.py
@@ -13,8 +13,6 @@ from cloudmesh.common.systeminfo import os_is_windows
 from cloudmesh.common.Shell import Shell
 import time
 
-
-
 dryrun = False
 
 exec = "python"
@@ -32,31 +30,29 @@ device = v["device"]
 error = False
 
 if user is None:
-  Console.error("user not set")
-  error = True
+    Console.error("user not set")
+    error = True
 
 if host is None:
-  Console.error("host not set")
-  error = True
+    Console.error("host not set")
+    error = True
 
 if cpu is None:
-  Console.error("cpu not set")
-  error = True
+    Console.error("cpu not set")
+    error = True
 
 if gpu is None:
-  Console.error("gpu not set")
-  error = True
+    Console.error("gpu not set")
+    error = True
 
 if device is None:
-  Console.error("device not set")
-  error = True
-
+    Console.error("device not set")
+    error = True
 
 tag = f"{host}-{user}-{cpu}-{gpu}"
 
 if error:
-  sys.exit()
-
+    sys.exit()
 
 scripts = textwrap.dedent("""
 mlp_mnist
@@ -73,39 +69,33 @@ mnist_autoencoder
 cards = v['gpu']
 
 if cards is None:
-  cards = ['v100', 'a100', 'k80', 'p100', 'rtx-2080']
+    cards = ['v100', 'a100', 'k80', 'p100', 'rtx-2080']
 else:
-  cards = v['gpu'].split(',')
+    cards = v['gpu'].split(',')
 for card in cards:
-  for script in scripts:
-    if exec == "papermill":
-      output = f"{script}-output"
-      command = f"{exec} {script}.ipynb {output}.ipynb"
-    elif exec == 'sbatch':
-      command = f"{exec} --gres=gpu:{card}:1 {script}.sh"
-    v['host']='rivanna'
-    v['gpu']=card
+    for script in scripts:
+        if exec == "papermill":
+            output = f"{script}-output"
+            command = f"{exec} {script}.ipynb {output}.ipynb"
+        elif exec == 'sbatch':
+            command = f"{exec} --gres=gpu:{card}:1 {script}.sh"
+        v['host'] = 'rivanna'
+        v['gpu'] = card
 
-    banner(command)
-    if not dryrun:
-      sbatch = os.system(command)
-    StopWatch.start(sbatch)
+        banner(command)
+        if not dryrun:
+            sbatch = os.system(command)
+        StopWatch.start(sbatch)
 
 waiting_for_squeue = False
-get_squeue = 'squeue -u $USER'
-while waiting_for_squeue:
-  r = os.system(get_squeue)
-  if user in r:
+get_squeue = 'squeue - $USER'
+r = os.system(get_squeue)
+while user in r:
     time.sleep(2)
+    r = os.system(get_squeue)
     continue
-  else:
-    waiting_for_squeue = True
-    break
 
 StopWatch.stop(sbatch)
 
-StopWatch.benchmark(sysinfo=False, tag=tag, node=host, user=user, filename=f"all-{tag}.log")
-
-
-
-
+StopWatch.benchmark(sysinfo=False, tag=tag, node=host, user=user,
+                    filename=f"all-{tag}.log")
