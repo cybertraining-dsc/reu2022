7dc57f9f1cb62ac0268d231b79832ef2078825ae
Alex Beck <abeckct@gmail.com>
2022-07-26 09:48:06 -0400

    Figured out all variables (user, host, CPU, GPU)sh: wq: command not found

--- code/deeplearning/mnist/run_all.py
+++ code/deeplearning/mnist/run_all.py
@@ -1,11 +1,16 @@
+import os
+import sys
+import torch
+import textwrap
+import socket
+import cpuinfo
 from cloudmesh.common.Shell import Shell
 from cloudmesh.common.util import banner
 from cloudmesh.common.StopWatch import StopWatch
 from cloudmesh.common.variables import Variables
 from cloudmesh.common.console import Console
-import textwrap
-import os
-import sys
+from cloudmesh.common.systeminfo import os_is_windows
+
 
 dryrun = False
 
@@ -43,41 +48,66 @@ if device is None:
   Console.error("device not set")
   error = True
 
-print (user)
+## USERNAME
+
+if os_is_windows():
+    user = os.environ["USERNAME"]
+else:
+    try:
+        user = os.environ['USER']
+    except:
+        user = os.system('basename $HOME')
+
+## HOST
+
+host = socket.gethostname()
+
+## GPU
+if torch.cuda.is_available() == True:
+    gpu = torch.cuda.get_device_name(0)
+else:
+    gpu='N/A'
+
+## CPU
+
+cpu = cpuinfo.get_cpu_info()['brand_raw']
+
+print(user)
 print(host)
 print(gpu)
 print(cpu)
 
 tag = f"{host}-{user}-{cpu}-{gpu}"
+print(tag)
 
 if error:
   sys.exit()
 
 
-scripts = textwrap.dedent("""
-mlp_mnist
-mnist_autoencoder
-mnist_cnn
-mnist_lstm
-mnist_mlp_with_lstm
-mnist_rnn
-mnist_with_distributed_training
-mnist_with_pytorch""").strip().splitlines()
-
-
-for script in scripts:
-  if exec == "papermill":
-    output = f"{script}-output"
-    command = f"{exec} {script}.ipynb {output}.ipynb"
-
-  StopWatch.start(script)
-  banner(command)
-  if not dryrun:
-    os.system(command)
-  StopWatch.stop(script)
-
-
-StopWatch.benchmark(sysinfo=False, tag=tag, node=host, user=user)
+# scripts = textwrap.dedent("""
+# mlp_mnist
+# mnist_autoencoder
+# mnist_cnn
+# mnist_lstm
+# mnist_mlp_with_lstm
+# mnist_rnn
+# mnist_with_distributed_training
+# mnist_with_pytorch""").strip().splitlines()
+#
+#
+# for script in scripts:
+#   if exec == "papermill":
+#     output = f"{script}-output"
+#     command = f"{exec} {script}.ipynb {output}.ipynb"
+#
+#   StopWatch.start(script)
+#   banner(command)
+#   if not dryrun:
+#     os.system(command)
+#   StopWatch.stop(script)
+#
+#
+# StopWatch.benchmark(sysinfo=False, tag=tag, node=host, user=user)
 
 
 
